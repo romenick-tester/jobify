47ec5593553183d9e5f368de1e027acb38fa8659
Romenick Tester <romenick.tester@gmail.com>
2022-06-21 22:55:19 +0100

    CLIENT: 'normalize.css' + index css

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "normalize.css";
+import "./assets/css/index.css";
 import App from "./App";
 
 const container = document.getElementById("root");
