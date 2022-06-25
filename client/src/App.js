a60ef04c7f5b86a564d44f4505ace7d74a48b7c5
Romenick Tester <romenick.tester@gmail.com>
2022-06-25 11:14:32 +0100

    005 CLIENT: react router v.6+ - initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import * as Pages from "./pages";
 
 
 const App = () => {
     return (
-        <>
-            <h1>Jobify App</h1>
-            <Pages.LandingPage />
-        </>
+        <Routes>
+            <Route path="/" element={<div>dashboard page</div>} />
+            <Route path="/signup" element={<div>sign-up page</div>} />
+            <Route path="/landing" element={<Pages.LandingPage />} />
+            <Route path="*" element={<div>error page</div>} />
+        </Routes>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import "normalize.css";
 import "./assets/css/index.css";
 import App from "./App";
@@ -8,5 +9,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 )
