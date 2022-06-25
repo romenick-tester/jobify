8b28d6c60b57e7131b3ff81b01b625f0210b11ca
Romenick Tester <romenick.tester@gmail.com>
2022-06-25 11:02:54 +0100

    004 CLIENT: minor refactor + logo component

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,13 +1,14 @@
-import logo from "../../assets/images/logo.svg";
+import React from "react";
 import main from "../../assets/images/main.svg";
 import Wrapper from "../../assets/wrappers/LandingPage";
+import { Logo } from "../../components";
 
 
 const Landing = () => {
     return (
         <Wrapper>
             <nav>
-                <img src={logo} alt="jobify" className="logo" />
+                <Logo />
             </nav>
             <div className="container page">
                 <div className="info">
