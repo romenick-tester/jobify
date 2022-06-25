f5ae92b35c75109306db5cea18c4b631c498bed6
Romenick Tester <romenick.tester@gmail.com>
2022-06-25 11:53:29 +0100

    006 CLIENT: implement link router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 import * as Pages from "./pages";
 
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<div>dashboard page</div>} />
-            <Route path="/signup" element={<div>sign-up page</div>} />
-            <Route path="/landing" element={<Pages.LandingPage />} />
-            <Route path="*" element={<div>error page</div>} />
-        </Routes>
+        <>
+            <Nav>
+                <Link to="/">dashboard</Link>
+                <Link to="/signup">sign-up</Link>
+                <Link to="/landing">landing</Link>
+            </Nav>
+            <Routes>
+                <Route path="/" element={<div>dashboard page</div>} />
+                <Route path="/signup" element={<div>sign-up page</div>} />
+                <Route path="/landing" element={<Pages.LandingPage />} />
+                <Route path="*" element={<div>error page</div>} />
+            </Routes>
+        </>
     )
 }
+
+const Nav = styled.nav`
+    >*:not(:last-child) {
+        margin-right: 1rem;
+    }
+`
 
 export default App;
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import main from "../../assets/images/main.svg";
 import Wrapper from "../../assets/wrappers/LandingPage";
 import { Logo } from "../../components";
@@ -14,7 +15,7 @@ const Landing = () => {
                 <div className="info">
                     <h1>job <span>tracking</span> app</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam repudiandae libero hic reiciendis sint, eligendi facilis adipisci autem ullam?</p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to="/signup" className="btn btn-hero">Login/Register</Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
