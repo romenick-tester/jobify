ca9a2d81f36d284e98f1673f79b058a05833fc01
Romenick Tester <romenick.tester@gmail.com>
2022-07-11 09:32:34 +0100

    038 SERVER: morgan

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import "express-async-errors";
 import cors from "cors";
+import morgan from "morgan";
 import notFound from "./middlewares/notFound.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import connectDB from "./db/connect.js";
@@ -11,6 +12,10 @@ import jobsRoutes from "./server-routes/jobs.js";
 dotenv.config();
 
 const server = express();
+
+if (process.env.NODE_ENV !== "production") {
+    server.use(morgan("dev"));
+}
 
 server.use(cors());
 server.use(express.json());
