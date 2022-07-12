bfd419db4a5ba36f51a03d577642acfeee459d77
Romenick Tester <romenick.tester@gmail.com>
2022-07-12 19:28:52 +0100

    048 CLIENT: protected route

--- client/src/pages/Dashboard/SharedLayout.js
+++ client/src/pages/Dashboard/SharedLayout.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Wrapper from "../../assets/wrappers/SharedLayout";
+import ProtectedRoute from "../ProtectedRoute";
 
 
 const SharedLayout = () => {
     return (
-        <Wrapper>
-            <nav>
-                <Link to="alljobs">all jobs</Link>
-                <Link to="addjob">add job</Link>
-            </nav>
-            <Outlet />
-        </Wrapper>
+        <ProtectedRoute>
+            <Wrapper>
+                <nav>
+                    <Link to="alljobs">all jobs</Link>
+                    <Link to="addjob">add job</Link>
+                </nav>
+                <Outlet />
+            </Wrapper>
+        </ProtectedRoute>
     )
 };
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import Landing from "./Landing/Landing";
 import Error from "./Error/Error";
 import Auth from "./Auth/Auth";
+import ProtectedRoute from "./ProtectedRoute";
 
 export {
     Landing,
     Error,
-    Auth
+    Auth,
+    ProtectedRoute
 }
