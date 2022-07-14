d673ccd8fd323e0be9ca28ca178347231da66ba2
Romenick <romenick.tester@gmail.com>
2022-07-14 22:26:09 +0100

    051 CLIENT: react icons

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Wrapper from "../../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 
 
 const Navbar = () => {
     return (
         <Wrapper>
-            <h1>navbar component</h1>
+            <h1>navbar component</h1> <FaHome />
         </Wrapper>
     )
 }
