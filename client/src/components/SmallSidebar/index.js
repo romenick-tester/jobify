0e0af65a42c8870969f84e1c1586850d0a69974c
Romenick <romenick.tester@gmail.com>
2022-07-16 16:02:09 +0100

    055 CLIENT: navbar - toggle close button

--- client/src/components/SmallSidebar/index.js
+++ client/src/components/SmallSidebar/index.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../../assets/context";
+import { links } from "../../assets";
 import Wrapper from "../../assets/wrappers/SmallSidebar";
+import Logo from "../Logo";
 
 
 const SmallSidebar = () => {
+    const { toggleSidebar, sidebar } = useAppContext();
+
     return (
         <Wrapper>
-            <h1>small sidebar component</h1>
+            <div className={`sidebar-container ${sidebar && "show-sidebar"}`}>
+                <div className="content">
+                    <button
+                        type="button"
+                        className="close-btn"
+                        onClick={toggleSidebar}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">nav links</div>
+                </div>
+            </div>
         </Wrapper>
     )
 }
