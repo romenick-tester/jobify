a4a980bbc50f95a332b91b461715acc6d90532ef
Romenick <romenick.tester@gmail.com>
2022-09-10 11:30:12 +0100

    108 CLIENT: install rechart and build bar chart component

--- client/src/pages/Dashboard/Stats/BarChart.js
+++ client/src/pages/Dashboard/Stats/BarChart.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 
-const BarChart = () => {
+const BarChartComponent = ({ data }) => {
+
     return (
-        <h1>BarChart Component</h1>
+        <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={data} margin={{ top: 50 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis allowDecimals="false" />
+                <Tooltip />
+                <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+            </BarChart>
+        </ResponsiveContainer>
     )
 };
 
-export default BarChart;
+export default BarChartComponent;
